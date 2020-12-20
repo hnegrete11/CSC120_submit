26fd16f76e73e6b392486eafb5d7595d5e1cc9d4
hnegrete11 <Antonionegrete.dst@gmail.com>
2020-12-20 13:21:26 +0530

    add

---
+++ BoolAlgebraCode_digitalGate/BoolAlgebra/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogicCircuit.Gates.Simple;
+
+namespace BoolAlgebra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = new List<IdentityInput>();
+            inputs.Add(new IdentityInput() { A = false, D = false });
+            inputs.Add(new IdentityInput() { A = false, D = true });
+            inputs.Add(new IdentityInput() { A = true, D = false });
+            inputs.Add(new IdentityInput() { A = true, D = true });
+            foreach (var item in inputs)
+            {
+                var andIdentity = new identity();
+                andIdentity.SetInputD = item.D;
+                andIdentity.SetInputA = item.A;
+
+                var orIdentity = new identity();
+                orIdentity.SetInputD = item.D;
+                orIdentity.SetInputA = item.A;
+
+                var notIdentity = new identity();
+                notIdentity.SetInputA = item.A;
+
+                var norIdentity = new identity();
+                norIdentity.SetInputD = item.D;
+                norIdentity.SetInputA = item.A;
+
+                var NANDIdentity = new identity();
+                NANDIdentity.SetInputD = item.D;
+                NANDIdentity.SetInputA = item.A;
+
+                var XorIdentity = new identity();
+                XorIdentity.SetInputD = item.D;
+                XorIdentity.SetInputA = item.A;
+
+                var XnorIdentity = new identity();
+                XnorIdentity.SetInputD = item.D;
+                XnorIdentity.SetInputA = item.A;
+
+                Console.WriteLine($"X = {andIdentity.SetInputA}\t" +
+                        $"Y = {andIdentity.SetInputD}\t" +
+                        $"[ x+y ] = {orIdentity.orValidate()}\t"+
+                        $"[ x*y ] = {orIdentity.andValidate()}\t" +
+                        $"[ ~x ] = {notIdentity.notValidate()}\t"+
+                        $"[ NAND(X,y) ] = {NANDIdentity.nandValidate()}\t" +
+                        $"[ NOR(X,Y)] = {norIdentity.norValidate()}\t" +
+                        $"[XOR(X,Y) ] = {XorIdentity.xorValidate()}\t" +
+                        $"[ XNOR(X,Y) ] = {XnorIdentity.xnorValidate()}\t");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BoolAlgebraCode_digitalGate/BoolAlgebra/identity.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogicCircuit.Gates.Simple;
+using LogicCircuit.Gates.Composite;
+namespace BoolAlgebra
+{
+    
+    class identity
+    {
+        public bool SetInputD { get; set; }
+        public bool SetInputA { get; set; }
+        public bool andValidate()
+        {
+            var logicCircuitAND = new AND();
+            logicCircuitAND.SetInputA(SetInputA);
+            logicCircuitAND.SetInputB(SetInputD);
+            return logicCircuitAND.Output;
+        }
+        public bool orValidate()
+        {
+            var logicCircuitOR = new OR();
+            logicCircuitOR.SetInputA(SetInputA);
+            logicCircuitOR.SetInputB(SetInputD);
+            return logicCircuitOR.Output;
+        }
+        public bool notValidate()
+        {
+            var logicCircuitNot = new NOT();
+            logicCircuitNot.SetInputA(SetInputA);
+            return logicCircuitNot.Output;
+        }
+        public bool norValidate()
+        {
+            var logicCircuitOR = new OR();
+            var notOut = new NOT();
+            logicCircuitOR.SetInputA(SetInputA);
+            logicCircuitOR.SetInputB(SetInputD);
+            notOut.SetInputA(logicCircuitOR.Output);
+            return notOut.Output;
+        }
+        public bool nandValidate()
+        {
+            var notOut = new NOT();
+            var logicCircuitAND = new AND();
+            logicCircuitAND.SetInputA(SetInputA);
+            logicCircuitAND.SetInputB(SetInputD);
+            notOut.SetInputA(logicCircuitAND.Output);
+            return notOut.Output;
+        }
+        public bool xnorValidate()
+        {
+
+            var logicCircuitOR = new OR();
+            var logicCircuitAND1 = new AND();
+            var logicCircuitAND2 = new AND();
+            var notOp = new NOT();
+            logicCircuitAND1.SetInputA(SetInputA);
+            logicCircuitAND1.SetInputB(SetInputD);
+
+            
+            notOp.SetInputA(SetInputA);
+            logicCircuitAND2.SetInputA(notOp.Output);
+            notOp.SetInputA(SetInputD);
+            logicCircuitAND2.SetInputB(notOp.Output);
+
+            logicCircuitOR.SetInputA(logicCircuitAND1.Output);
+            logicCircuitOR.SetInputB(logicCircuitAND2.Output);
+            return logicCircuitOR.Output;
+        }
+        public bool xorValidate()
+        {
+            var logicCircuitOR = new OR();
+            var logicCircuitAND1 = new AND();
+            var logicCircuitAND2 = new AND();
+            var notOp = new NOT();
+            notOp.SetInputA(SetInputA);
+            logicCircuitAND1.SetInputA(notOp.Output);
+            logicCircuitAND1.SetInputB(SetInputD);
+            
+
+            notOp.SetInputA(SetInputD);
+            logicCircuitAND2.SetInputA(notOp.Output);
+            logicCircuitAND2.SetInputB(SetInputA);
+            logicCircuitOR.SetInputA(logicCircuitAND1.Output);
+            logicCircuitOR.SetInputB(logicCircuitAND2.Output);
+            return logicCircuitOR.Output;
+        }
+    }
+}
