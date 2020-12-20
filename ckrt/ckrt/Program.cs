3e5198d0bc0035c5380c81cf4b86e42140347fd3
hnegrete11 <Antonionegrete.dst@gmail.com>
2020-12-20 20:00:25 +0530

    Signed-off-by: hnegrete11 <Antonionegrete.dst@gmail.com>

---
+++ ckrt/ckrt/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ckrt
+{
+    class Program
+    {
+        private string strFilePath = @"Data.csv";
+        static void Main(string[] args)
+        {
+
+            StringBuilder sbOutput = new StringBuilder();
+            sbOutput.Append(" " + ",");
+            for (int i = 1; i < 11; i++)
+            {
+                sbOutput.Append(i * 1000 + ",");
+            }
+            sbOutput.AppendLine();
+            File.WriteAllText(@"Data.csv", sbOutput.ToString());
+            for (int i = 5; i <= 50; i += 5)
+            {
+                var crkt = new circuitparam(i);
+                crkt.simulation();
+                crkt.toData();
+            }
+
+        }
+    }
+}
---
+++ ckrt/ckrt/circuitparam.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ckrt
+{
+    class circuitparam
+    {
+        private double volt;
+        private double[] outp;
+        private int[] resist;
+        private double refRest = 5000.0;
+        private string strFilePath = @"Data.csv";
+        public circuitparam(double volt)
+        {
+            this.volt = volt;
+            this.outp= new double[10];
+
+            this.resist = new int[10];
+            
+            
+            for (int i=1;i<11;i++)
+            {
+                this.resist[i-1] = i*1000;
+                
+            }
+            
+        }
+        public void simulation()
+        {
+           for(int i =0; i <10;i++)
+            {
+                outp[i] = this.volt *(this.resist[i] / (refRest+this.resist[i]));
+            }
+        }
+        public void toData()
+        {
+
+            StringBuilder sbOutput = new StringBuilder();
+            sbOutput.Append(this.volt + ",");
+            for (int i = 0; i < 10; i++)
+            {
+                sbOutput.Append(outp[i]+",");
+            }
+            sbOutput.AppendLine();
+            File.AppendAllText(strFilePath, sbOutput.ToString());
+        }
+    }
+}
